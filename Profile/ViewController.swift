d8673ed3714f8376ec34ff42ef378bb44e12a49d
HayatoOi <hyt7778to@gmail.com>
2023-05-11 03:10:56 +0900

    発展

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -10,20 +10,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var profileLabel: UILabel!
+    
     @IBOutlet var profilecommentLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        profileImageView.isHidden = true
-        profilecommentLabel.isHidden = true
-        
     }
 
     @IBAction func tapButton1(){
-        profileImageView.isHidden = false
-        profilecommentLabel.isHidden = false
+        profileImageView.image = UIImage(named: "philImage")
+        profileLabel.text = "名前"
+        
+        profilecommentLabel.text = "iPhoneメンターのフィルだよ"
+    }
+    
+    @IBAction func tapButton2(){
+        profileImageView.image = UIImage(named: "trackImage")
+        profileLabel.text = "スポーツ"
+        
+        profilecommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
     }
 
+    @IBAction func tapButton3(){
+        profileImageView.image = UIImage(named: "appleImage")
+        profileLabel.text = "好きな食べ物"
+        
+        profilecommentLabel.text = "リンゴが好きで、いつも持ち歩いている"
+    }
+
+    @IBAction func tapButton4(){
+        profileImageView.image = UIImage(named: "flightImage")
+        profileLabel.text = "趣味"
+        
+        profilecommentLabel.text = "飛行機に乗って、空を散歩すること"
+    }
 }
 
